a123397d9cf61c3cf141eaa3b75718060e48d232
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-10-07 13:33:16 +1000

    Add prac 4 username access program

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,8 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter password: ")
+while username not in usernames:
+    print("Access denied")
+    username = input("Enter password: ")
+print("Access granted")
