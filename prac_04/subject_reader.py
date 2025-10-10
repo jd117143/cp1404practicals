b0d36037c43d699c371a028d021ab61cd5382f0c
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-10-10 10:47:12 +1000

    Align subject table columns dynamically

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -11,27 +11,28 @@ def main():
     display_subjects(subjects)
 
 
-def display_subjects(subjects):
-    for subject in subjects:
-        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
-
-
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     subjects = []
     input_file = open(filename)
     for line in input_file:
-        # print(line)  # See what a line looks like
-        # print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        # print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        # print(parts)  # See if that worked
-        # print("----------")
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])
         subjects.append(parts)
     input_file.close()
     return subjects
+
+
+def display_subjects(subjects):
+    max_subject_len = max(len(subject[0]) for subject in subjects)
+    max_teacher_len = max(len(subject[1]) for subject in subjects)
+    max_students_len = max(len(str(subject[2])) for subject in subjects)
+
+    for subject in subjects:
+        print(f"{subject[0]:{max_subject_len}} is taught by "
+              f"{subject[1]:{max_teacher_len}} and has "
+              f"{subject[2]:{max_students_len}} students")
 
 
 main()
