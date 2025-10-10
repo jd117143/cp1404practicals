888e1c1c96391fd02e431c476df08ee8c9f86104
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-10-10 10:15:58 +1000

    Add prac 4 quick picks generator with basic random number generation

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,22 @@
+from random import randint
+
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+LINE_NUMBER = 6
+
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    for i in range(number_of_quick_picks):
+        quick_picks = []
+        generate_numbers(quick_picks)
+        print(" ".join(f"{number:2}" for number in sorted(quick_picks)))
+
+
+def generate_numbers(quick_picks):
+    for j in range(LINE_NUMBER):
+        number = randint(MIN_NUMBER, MAX_NUMBER)
+        quick_picks.append(number)
+
+
+main()
