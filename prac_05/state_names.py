d512ff6eeaffa9bdf30d76ea742e4bd5d37231ec
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-10-14 13:17:58 +1000

    Fix prac 5 state names variable naming

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,8 +8,8 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
 print(CODE_TO_NAME)
 print('\n')
 
-for code, name in CODE_TO_NAME.items():
-    print(f"{code:3} is {name}")
+for state_code, state_name in CODE_TO_NAME.items():
+    print(f"{state_code:3} is {state_name}")
 print('\n')
 
 state_code = input("Enter short state: ").upper()
