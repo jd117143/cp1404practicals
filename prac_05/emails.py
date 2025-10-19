396bcd49ed370501f8a70f46710fb2ffdd91711d
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-10-19 21:16:54 +1000

    Refactor name extraction logic into a separate function for prac 5 emails exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,12 +7,11 @@ Actual:
 
 
 def main():
+    """Store a dictionary of emails to names and display them."""
     email_to_name = {}
     email = input("Email: ")
     while email != "":
-        username = email.split('@')[0]
-        parts = username.split('.')
-        name = ' '.join(parts).title()
+        name = extract_name(email)
         name_check = input(f"Is your name {name} (Y/n) ").upper()
         if name_check != 'Y':
             name = input("Name: ")
@@ -21,6 +20,14 @@ def main():
 
     for email, name in email_to_name.items():
         print(f"{name} ({email})")
+
+
+def extract_name(email):
+    """Extract name from the given email address."""
+    username = email.split('@')[0]
+    parts = username.split('.')
+    name = ' '.join(parts).title()
+    return name
 
 
 main()
