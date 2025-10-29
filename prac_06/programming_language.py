87035690b88549854e1dc850db4804176754e01b
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-10-29 18:58:28 +1000

    Add is_dynamic method to prac 6 ProgrammingLanguage class module

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -21,3 +21,6 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
