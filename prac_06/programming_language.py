10af9af364fdd8e35fddea405c800b2182ad7864
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-10-29 20:19:00 +1000

    Add docstring for is_dynamic() method in prac 6 ProgrammingLanguage class module

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,7 +2,7 @@
 CP1404 - Practical 6
 ProgrammingLanguage class module.
 Estimate: 20 minutes
-Actual:
+Actual: 24 minutes
 """
 
 
@@ -27,4 +27,5 @@ class ProgrammingLanguage:
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
+        """Return True if the language uses dynamic typing."""
         return self.typing.lower() == "dynamic"
