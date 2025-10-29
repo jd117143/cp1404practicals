75568f4dd1220083cd11291fea54dca8f0748ea0
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-10-29 19:03:07 +1000

    Add client code to test prac 6 ProgrammingLanguage class module

---
+++ prac_06/languages.py
@@ -0,0 +1,10 @@
+"""
+CP1404 - Practical 6
+ProgrammingLanguage client code.
+"""
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
