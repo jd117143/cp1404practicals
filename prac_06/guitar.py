36496044a08695b71944971ac3d94b661730251c
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-10-30 16:27:37 +1000

    Add constructor method to prac 6 Guitar class module

---
+++ prac_06/guitar.py
@@ -0,0 +1,13 @@
+"""
+CP1404 - Practical 6
+Guitar class module.
+Estimate: 15 minutes
+Actual:
+"""
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
