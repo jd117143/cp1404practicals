1a97fff9d674e014f7f3b71a9b86b69a637e8a85
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-10-30 16:58:38 +1000

    Add basic test code for prac 6 Guitar class module

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,16 @@
+"""
+CP1404 - Practical 6
+Guitar class basic test code.
+Estimate: 10 minutes
+Actual:
+"""
+
+from prac_06.guitar import Guitar
+
+gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+line = Guitar("Line 6 JTV-59", 2010, 1512.9)
+
+print(f"{gibson.name} get_age() - Expected {103}. Got {gibson.get_age()}")
+print(f"{line.name} get_age() - Expected {15}. Got {line.get_age()}")
+print(f"{gibson.name} is_vintage() - Expected {True}. Got {gibson.is_vintage()}")
+print(f"{line.name} is_vintage() - Expected {True}. Got {line.is_vintage()}")
