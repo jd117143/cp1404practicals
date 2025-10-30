af20922556aedc6b9aa74444bed9c8ddc50e8978
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-10-30 17:33:33 +1000

    Add formatted output in main() of prac 6 Guitar program to display guitars with vintage status

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -15,12 +15,20 @@ def main():
 
     name = input("Name: ")
     while name != "":
-        year = input("Year: ")
-        cost = input("Cost: $")
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
         guitar = Guitar(name, year, cost)
         guitars.append(guitar)
         print(f"{guitar} added.\n")
         name = input("Name: ")
+
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+        else:
+            vintage_string = ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 
 if __name__ == "__main__":
