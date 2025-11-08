6667ef4f66f886bb087ebe790c14534183fa69b7
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-08 19:26:12 +1000

    Load guitars from .csv file and display in prac 7 guitar program

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,3 +2,25 @@
 CP1404 - Practical 7
 Guitar program.
 """
+
+from prac_07.guitar import Guitar
+
+
+def main():
+    """Run Guitar program."""
+    guitars = []
+    in_file = open('guitars.csv')
+    in_file.readline()
+
+    for line in in_file:
+        parts = line.strip().split(',')
+        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+        guitars.append(guitar)
+    in_file.close()
+
+    for guitar in guitars:
+        print(guitar)
+
+
+if __name__ == "__main__":
+    main()
