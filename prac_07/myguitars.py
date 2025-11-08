1fd282664e4dff1a755ffd0abc0a76677112e03b
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-08 19:41:50 +1000

    Refactor file loading into a separate function from main() in prac 7 guitar program

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -9,14 +9,7 @@ from prac_07.guitar import Guitar
 def main():
     """Run Guitar program."""
     guitars = []
-    in_file = open('guitars.csv')
-    in_file.readline()
-
-    for line in in_file:
-        parts = line.strip().split(',')
-        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
-        guitars.append(guitar)
-    in_file.close()
+    load_guitars(guitars)
 
     add_new_guitar(guitars)
 
@@ -24,6 +17,16 @@ def main():
 
     for guitar in guitars:
         print(guitar)
+
+
+def load_guitars(guitars):
+    in_file = open('guitars.csv')
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split(',')
+        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+        guitars.append(guitar)
+    in_file.close()
 
 
 def add_new_guitar(guitars):
