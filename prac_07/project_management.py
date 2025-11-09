7fa59a9db0f69d89bcef246b72ab80f7d0e063aa
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-09 16:42:15 +1000

    Add function to load projects from CSV file in prac 7 project management program

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -24,12 +24,14 @@ MENU = """
 def main():
     """Run the Project Management program."""
     print("Welcome to Project Manager")
+    projects = load_projects()
 
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            pass
+            filename = input("Filename: ")
+            projects = load_projects(filename)
         elif choice == "S":
             pass
         elif choice == "D":
@@ -45,6 +47,25 @@ def main():
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you for using custom-built project management software.")
+
+
+def load_projects(filename=FILENAME):
+    """Load projects from CSV file."""
+    projects = []
+    with open(filename) as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split("\t")
+            name = parts[0]
+            start_date = datetime.datetime.strptime(parts[1], "%d/%m/%Y").date()
+            priority = int(parts[2])
+            cost_estimate = float(parts[3])
+            completion_percentage = int(parts[4])
+
+            project = Project(name, start_date, priority, cost_estimate, completion_percentage)
+            projects.append(project)
+        print(f"Loaded {len(projects)} from {filename}")
+    return projects
 
 
 if __name__ == '__main__':
