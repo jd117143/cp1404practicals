b2fe34a1c174f8e2c25df6f32ee0d2332921243e
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-09 17:04:44 +1000

    Add is_complete() method to prac 7 Project class module

--- prac_07/project.py
+++ prac_07/project.py
@@ -21,7 +21,11 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
-        """Return a string representation of a Guitar object."""
+        """Return a string representation of a Project object."""
         start_date_string = self.start_date.strftime("%d/%m/%Y")
         return (f"{self.name}, start: {start_date_string}, priority {self.priority}"
                 f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
+
+    def is_complete(self):
+        """Return True if the project is completed."""
+        return self.completion_percentage == 100
