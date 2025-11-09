09be770b37e6c23f665166218f779c6ff56c74c3
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-09 16:11:59 +1000

    Add __str__ method to prac 7 Project class module

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,6 +6,8 @@ Actual:
 (Time details for Project class module + project management combined)
 """
 
+import datetime
+
 
 class Project:
     """Represent a Project object with key characteristics."""
@@ -16,3 +18,9 @@ class Project:
         self.start_date = start_date
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Return a string representation of a Guitar object."""
+        start_date_string = self.start_date.strftime("%d/%m/%Y")
+        return (f"{self.name}, start: {start_date_string},"
+                f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
