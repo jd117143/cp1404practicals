be6e5a9a1d05a55112d2b27ee4744b34cfeb8f93
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-09 16:15:22 +1000

    Add priority attribute to prac 7 Project class constructor method

--- prac_07/project.py
+++ prac_07/project.py
@@ -12,15 +12,16 @@ import datetime
 class Project:
     """Represent a Project object with key characteristics."""
 
-    def __init__(self, name, start_date, cost_estimate, completion_percentage):
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         """Initialise a Project object."""
         self.name = name
         self.start_date = start_date
+        self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
 
     def __str__(self):
         """Return a string representation of a Guitar object."""
         start_date_string = self.start_date.strftime("%d/%m/%Y")
-        return (f"{self.name}, start: {start_date_string},"
+        return (f"{self.name}, start: {start_date_string}, priority {self.priority}"
                 f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
