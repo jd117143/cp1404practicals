d82cfb30578e2b4d623dbd3c22874b4a8ac5e9d5
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-11 12:41:17 +1000

    Add get_valid_number() function to validate input for prac 8 MilesToKilometresApp exercise

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -19,15 +19,23 @@ class MilesToKilometresApp(App):
         return self.root
 
     def handle_calculate(self):
-        """Handle calculation (could be button press or other call), output result to label widget"""
-        value = self.root.ids.input_number.text
-        result = float(value) * 1.6093
+        """Handle calculation (could be button press or other call), output result to label widget."""
+        value = self.get_valid_number()
+        result = value * 1.6093
         self.output_text = str(result)
 
     def handle_increment(self, increment):
-        """Handle up/down button press, update text input"""
-        value = float(self.root.ids.input_number.text) + increment
+        """Handle up/down button press, update text input."""
+        value = self.get_valid_number() + increment
         self.root.ids.input_number.text = str(value)
+
+    def get_valid_number(self):
+        """Get text input value, return as a float if valid and 0.0 if invalid."""
+        try:
+            value = float(self.root.ids.input_number.text)
+            return value
+        except ValueError:
+            return 0.0
 
 
 MilesToKilometresApp().run()
