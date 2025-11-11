eaede00bd26ee9169d5afb0c5e662e7f9e026bb2
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-11 11:07:12 +1000

    Use StringProperty to automatically update label text for prac 8 MilesToKilometresApp exercise

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -5,16 +5,21 @@ Kivy GUI program to convert miles to kilometres
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 
 class MilesToKilometresApp(App):
     """MilesToKilometres is a Kivy App for converting miles to kilometres"""
+    message = StringProperty()
 
     def build(self):
         """Build the Kivy App from the .kv file"""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_calculate(self):
+        self.message = self.root.ids.input_number.text
 
 
 MilesToKilometresApp().run()
