8550febba009dc017a03ab8a1b6eae2bb5d52464
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-11 13:35:42 +1000

    Add constructor method to Python file for prac 8 DynamicLabelsApp

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -5,10 +5,16 @@ Kivy GUI program to create dynamic labels
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.label import Label
 
 
 class DynamicLabelsApp(App):
     """DynamicLabelsApp is a Kivy App to create dynamic labels."""
+
+    def __init__(self, **kwargs):
+        """Construct main Kivy App."""
+        super().__init__(**kwargs)
+        self.name = ["Bob Brown", "Cat Cyan", "Oren Ochre"]
 
     def build(self):
         """Build the Kivy App from the .kv file."""
