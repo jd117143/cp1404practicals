fffbc81c38a7cf92bdeb4088eee89e6160530c03
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-18 09:54:50 +1000

    Override get_fare in prac 9 SilverServiceTaxi class to include "flagfall" variable

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -7,7 +7,15 @@ from prac_09.taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
+    """Represent a SilverServiceTaxi object."""
+    flagfall = 4.50
+
     def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi, based on parent class Taxi."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
+
+    def get_fare(self):
+        """Return the price for the trip including flagfall."""
+        return self.flagfall + super().get_fare()
