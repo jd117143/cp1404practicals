8fd3394a528ae2376334823b7e19e812244249d4
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-18 10:54:06 +1000

    Add docstring to __str__ method of prac 9 SilverServiceTaxi class

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -17,6 +17,7 @@ class SilverServiceTaxi(Taxi):
         self.price_per_km = Taxi.price_per_km * fanciness
 
     def __str__(self):
+        """Return a string representation of a SilverServiceTaxi."""
         return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
 
     def get_fare(self):
