caf170ffc42ca8d0e9f89f262418654a813f8870
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-18 11:08:23 +1000

    Add initial structure for prac 9 Band class

---
+++ prac_09/band.py
@@ -0,0 +1,25 @@
+"""
+CP1404 - Practical 9
+Band class.
+"""
+
+
+class Band:
+    """Represent a Band object."""
+
+    def __init__(self, name=""):
+        """Initialise a Band."""
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        """Return a string representation of a Band."""
+        return f"{self.name} ({self.musicians})"
+
+    def add(self, musician):
+        """Add a musician to the band."""
+        self.musicians.append(musician)
+
+    def play(self):
+        """Return a string showing each musician playing their first (or no) instrument."""
+        return
