1e7581aa3563104f9edf1d5b799100efd23fc0d5
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-22 20:54:43 +1000

    Add functionality to drive_taxi in prac 9 taxi simulator program, using Taxi methods

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -12,6 +12,8 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 def main():
     """Run taxi simular program via menu."""
     current_taxi = None
+    bill_to_date = 0.0
+
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
 
@@ -21,9 +23,11 @@ def main():
         if choice == "C":
             current_taxi = choose_taxi(taxis)
         elif choice == "D":
-            pass
+            trip_cost = drive_taxi(current_taxi)
+            bill_to_date += trip_cost
         else:
             print("Invalid option")
+        print(f"Bill to date: ${bill_to_date:.2f}")
         print(MENU)
         choice = input(">>> ").upper()
 
@@ -34,14 +38,23 @@ def choose_taxi(taxis):
     display_taxis(taxis)
     try:
         taxi_choice = int(input("Choose taxi: "))
-        return taxi_choice
+        return taxis[taxi_choice]
     except (ValueError, IndexError):
         print("Invalid taxi choice")
 
 
-def drive_taxi():
+def drive_taxi(current_taxi):
     """Drive chosen taxi."""
-    pass
+    if current_taxi:
+        current_taxi.start_fare()
+        distance = float(input("Drive how far? "))
+        current_taxi.drive(distance)
+        trip_cost = current_taxi.get_fare()
+        print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
+        return trip_cost
+    else:
+        print("You need to choose a taxi before you can drive")
+        return 0
 
 
 def display_taxis(taxis):
