0b9935f0d619e808610ddb412c4a993eff3c9680
jd117143 <lord.ciencia@my.jcu.edu.au>
2025-11-29 20:50:50 +1000

    Add exception handling to prac 10 wiki API exercise

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -2,15 +2,21 @@
 CP1404 - Practical 10
 Wikipedia API exercise
 """
-from wikipedia import wikipedia
+import wikipedia
 
 title = input("Enter page title: ").strip()
 while title:
-    page = wikipedia.page(title, auto_suggest=False)
-    print(page.title)
-    first_paragraph = page.summary.split("\n")[0]
-    print(first_paragraph)
-    print(page.url)
+    try:
+        page = wikipedia.page(title, auto_suggest=False)
+        print(page.title)
+        first_paragraph = page.summary.split("\n")[0]
+        print(first_paragraph)
+        print(page.url)
+    except wikipedia.exceptions.DisambiguationError as e:
+        print("We need a more specific title. Try one of the following, or a new search:")
+        print(e.options)
+    except wikipedia.exceptions.PageError:
+        print(f'Page id "{title}" does not match any pages. Try another id!')
     print()
     title = input("Enter page title: ").strip()
 print("Thank you.")
